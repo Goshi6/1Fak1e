a20d72d5d645bbf383c969378006881f8fb9e09c
Goshi6 <goshi6you6tube@gmail.com>
2025-12-02 15:10:41 +0300

    1

--- backend/app/Auth/config.py
+++ backend/app/Auth/config.py
@@ -1,5 +1,5 @@
 # app/Auth/config.py
-FACEIT_CLIENT_ID = "b6cb9ae7-ac13-47fe-8324-c0a1f1266cad"  # ВАШ АКТУАЛЬНЫЙ ID из Faceit, обязательно строкой!
-FACEIT_CLIENT_SECRET = "TksMyNlwweDWHvoej7sl8qWjJJarxr34DjJRrDmC"  # ВАШ АКТУАЛЬНЫЙ SECRET из Faceit
+FACEIT_CLIENT_ID = "b3fb1fc9-a609-4aa8-84ac-871ba62db108"  # ВАШ АКТУАЛЬНЫЙ ID из Faceit, обязательно строкой!
+FACEIT_CLIENT_SECRET = "JgAJ1XNxyo5Iw8xsYromvQtLQNnpa8vcutXlSzMV"  # ВАШ АКТУАЛЬНЫЙ SECRET из Faceit
 FACEIT_REDIRECT_URI = "https://api.fak1e-lab.ru/auth/faceit/callback"
 
