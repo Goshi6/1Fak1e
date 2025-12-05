d3436a4bc1135439d960d832f6fb25e11e2125c0
Goshi6 <goshi6you6tube@gmail.com>
2025-12-05 07:41:42 +0300

    1

--- backend/create_tables.py
+++ backend/create_tables.py
@@ -1,9 +1,13 @@
 import asyncio
 from app.db import engine, Base
+# ВАЖНО: импортировать модели, чтобы они зарегистрировались в Base.metadata
+from app import models  # noqa: F401
+
 
 async def main():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
